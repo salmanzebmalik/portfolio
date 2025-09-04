fe72e5ce7094b8564710870fdb5401671a365afe
Muhammad Salman Malik <muhammadsalmanmalik@Mac.fritz.box>
2025-09-04 15:54:09 +0200

    Home & Services Page

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,51 @@
-export default function Home() {
-  return <main>homepage</main>;
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi"
+
+// Components
+import Social from "@/components/Social";
+import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
+
+const home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text  */}
+          <div className="text-center xl:text-left order-2 xl:order-none">
+            <span className="text-xl">Machine Learning Engineer</span>
+            <h1 className="h1 mb-6">
+              Hallo, Ich bin <br /> <span className="text-accent">Muhammad Salman Malik</span>
+            </h1>
+            <p className="mx-auto max-w-[500px] mb-9 text-white/80 text-center xl:mx-0 xl:text-left">
+              I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies.
+            </p>
+            {/* Button and Socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  constainerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
+              </div>
+            </div>
+          </div>
+          {/* photo  */}
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
+        </div>
+        <Stats />
+      </div>
+    </section>
+  )
 }
+
+export default home;
